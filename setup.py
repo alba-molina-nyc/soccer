3ae7d1e9fd8b312d044a8965f08e12fa206a3fb5
avmanyc <avmanyc@gmail.com>
2022-12-17 10:33:28 -0500

    flushed out data

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ groups = data['response']
 for group in groups:
     team = group.get('team')
     team_id = team.get('id')
+    team_name = team.get('name')
+    team_logo = team.get('logo')
     players = group.get('players')
-    
 
 for player in players:
     statistics = player.get('statistics')
@@ -59,4 +60,3 @@ for statistic in statistics:
     penalty_saved = penalty.get('saved')
     minutes = games.get('minutes')
 
-# print(minutes)
